4fe728a26cfa2063fe3192bddcd8d3dcfa1684d8
Laura Spieker <ljspiek@gmail.com>
2020-07-19 11:37:44 -0500

    w/ reverse loop, splice, and push

---
+++ pushSplice.js
@@ -0,0 +1,11 @@
+const moveZeros = (arr) => {
+  for (var i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === 0) {
+      arr.splice(i, 1);
+      arr.push(0);
+    }
+  }
+  return arr;
+};
+
+console.log(moveZeros([false, 1, 0, 1, 2, 0, 1, 3, "a"]));
